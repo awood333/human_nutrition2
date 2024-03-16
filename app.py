73f737fc5d3939d59000a62cf59f36c6300e6b38
alan woodhull <89449956+awood333@users.noreply.github.com>
2024-03-16 18:17:26 +0700

    works!

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
+'''app.py'''
 import pandas as pd
+import json
 import dash
-from dash import html, Input, Output, State, callback_context
+from dash import html, Input, Output, State, callback_context, dcc
 from dash.exceptions import PreventUpdate
 from layout import Layout
 
 nutrition_db1 = pd.read_csv('E:\\old_F_drive\\human_nutrition\\nutrition_table.csv', index_col=None, header=0)
 nutrition_db = nutrition_db1.iloc[:,:-1].copy()
+numeric_columns = ['calories', 'carb', 'fat', 'protein', 'fiber', 'GI']
+
 # unique names in description_1 column
 dd1_options = [{'label': desc, 'value': desc} for desc in nutrition_db['description_1'].unique()]
 
 app = dash.Dash(__name__)
 
 layout = Layout(nutrition_db, dd1_options) 
-
 app.layout = layout.create_layout()
 
 # Update options for dd2 based on selected value from dd1
@@ -30,7 +33,7 @@ def update_dd2_options(selected_value):
 
 # Display the selected rows in a container
 @app.callback(
-    Output('output-container', 'children'),
+    Output('output-container-dd1', 'children'),
     [Input('dd1', 'value')]
 )
 def display_selected_rows(selected_value):
@@ -49,32 +52,79 @@ def display_selected_rows(selected_value):
         ])
     ])
 
-# Add selected row from dd2 to the menu
+# Consolidate the functionality of both callbacks into a single callback
 @app.callback(
-    Output('menu-table', 'data'),
-    [Input('add-to-menu-btn', 'n_clicks')],
-    [State('portion-input', 'value'), 
+    [Output('menu-table', 'data'),
+     Output('output-container-dd2', 'children')],
+    [Input('add-to-menu-btn', 'n_clicks'),
+     Input('calculate-totals-btn', 'n_clicks')],
+    [State('portion-input', 'value'),
      State('dd2', 'value'),
-     State('menu-table', 'data')]
+     State('menu-table', 'data'),
+     State('json-type', 'value')]
 )
-def add_to_menu(n_clicks, portion_input, selected_row, current_data):
-    if not n_clicks or selected_row is None:
-        raise PreventUpdate
-    
-    selected_row_data = nutrition_db[(nutrition_db['description_2'] == selected_row)]
-    if len(selected_row_data) == 0: 
-        raise PreventUpdate
-    
-    selected_row_data_copy = selected_row_data.copy()
+def handle_menu_operations(add_to_menu_clicks, calculate_totals_clicks,
+                            portion_input, selected_row, current_data, json_type):
+    ctx = callback_context
 
-    selected_row_data_copy['portion'] = portion_input
-    selected_row_data_copy.iloc[:, 3:8] *= portion_input #elemntwise multiply col by scalar
+    # If neither button is clicked, prevent update
+    if not ctx.triggered:
+        raise PreventUpdate
+
+    # Determine which button triggered the callback
+    triggered_id = ctx.triggered[0]['prop_id'].split('.')[0]
+
+    # add to menu
+    if triggered_id == 'add-to-menu-btn' and add_to_menu_clicks:
+        if selected_row is None:
+            raise PreventUpdate
         
-    current_data.append(selected_row_data_copy.iloc[0].to_dict())
+        selected_row_data = nutrition_db[(nutrition_db['description_2'] == selected_row)]
+        if len(selected_row_data) == 0: 
+            raise PreventUpdate
+        
+        selected_row_data_copy = selected_row_data.copy()
 
-    return current_data
+        selected_row_data_copy['portion'] = portion_input
+        selected_row_data_copy.iloc[:, 3:8] *= portion_input  # Elementwise multiply col by scalar
 
-    print('current_data dtype', type(current_data))
+        idx = len(current_data) + 1
+        selected_row_data_copy['Index'] = idx
+
+        updated_columns = selected_row_data_copy.iloc[:,3:8] * portion_input
+        selected_row_data_copy.iloc[:,3:8] = updated_columns
+
+        totals_row = {'Description 1': 'Total', 'Description 2': '', 
+                      'Portion': '', 'calories': 0, 'carb': 0, 
+                      'fat': 0, 'protein': 0, 'fiber': 0, 'GI': 0}
+
+        current_data.append(selected_row_data_copy.iloc[0].to_dict())  # DataTable expects a JSON serializable dtype
+        return current_data, None  # Return the updated menu data and no message for output container
+
+    # Handle calculating totals and appending
+    elif triggered_id == 'calculate-totals-btn' and calculate_totals_clicks:
+        totals_row = calculate_totals_and_append(calculate_totals_clicks, current_data, json_type)
+        return totals_row[0], html.Div(totals_row[1])
+
+    # If no button is clicked, prevent update
+    else:
+        raise PreventUpdate
+
+def calculate_totals_and_append(n_clicks, current_data, json_type):
+    totals_row = {'Description 1': 'Total', 'Description 2': '', 
+                  'Portion': '', 'calories': 0, 'carb': 0, 
+                  'fat': 0, 'protein': 0, 'fiber': 0, 'GI': 0}
+
+    for row in current_data:
+        for col in numeric_columns:
+            if col != 'Portion' and row[col] is not None:
+                totals_row[col] += float(row[col])
+
+    filename = f'{json_type}.json'
+    with open(filename, 'a') as json_file:
+        json.dump(current_data, json_file)
+
+    return current_data + [totals_row], f'Appended to {json_type}.json'
 
 if __name__ == '__main__':
     app.run_server(debug=True, port=8081)
--- layout.py
+++ layout.py
@@ -3,43 +3,75 @@ import dash
 from dash import html, dcc, dash_table
 
 class Layout:
-    def __init__(self, nutrition_db, dd1_options): #, dd2_options
+    def __init__(self, nutrition_db, dd1_options):
         self.nutrition_db = nutrition_db
         self.dd1_options = dd1_options
-        # self.dd2_options = dd2_options
 
     def create_layout(self):
+
+        menu_data = self.nutrition_db.copy()
+        menu_data['Select'] = False
+
         layout = html.Div([
             dcc.Dropdown(
-                id          = 'dd1',
-                options     = self.dd1_options,
-                value       = None,  # Default value is None
-                multi       = False,  # Single selection
-                clearable   = False
+                id='dd1',
+                options=self.dd1_options,
+                value=None,
+                multi=False,
+                clearable=False
             ),
-            html.Div(id='output-container'),
+            html.Div(id='output-container-dd1'),
             
             dcc.Dropdown(
-                id          = 'dd2',
-                options     = [],               #default
-                value       = None,             # Default value is None
-                multi       = False,            # Single selection
-                clearable   = False
+                id='dd2',
+                options=[],              
+                value=None,             
+                multi=False,            
+                clearable=False
             ),
-            dcc.Input(id            = 'portion-input', 
-                      type          = 'number', 
-                      placeholder   = 'Enter portion', 
-                      value         = 1),
+            html.Div(id='output-container-dd2'),
+
+            dcc.Input(
+                id='portion-input', 
+                type='number', 
+                placeholder='Enter portion', 
+                value=1
+            ),
 
             html.Button("Add to Menu", id="add-to-menu-btn"),
+            html.Button("Calculate totals and append ", id="calculate-totals-btn"),
             
             html.Div(id="menu-container", children=[
                 dash_table.DataTable(
                     id='menu-table',
-                    columns=[{"name": i, "id": i, "editable": True} if i == "portion" else {"name": i, "id": i, "editable": False} for i in self.nutrition_db.columns],
-                    data=[]
+                    columns=[
+                        # {"name": "Select", "id": "Select", "presentation": "dropdown"},
+                        # {"name": "Index", "id": "Index", "editable": False},
+                        {"name": "Description 1", "id": "description_1", "editable": False},
+                        {"name": "Description 2", "id": "description_2", "editable": False},
+                        {"name": "Portion", "id": "portion", "editable": True},
+                        {"name": "calories", "id": 'calories'},
+                        {"name": "carb", "id": "carb"},
+                        {"name": "fat", "id":"fat"},
+                        {"name": "protein", "id": "protein"},
+                        {"name": "fiber", "id": "fiber"},
+                        {"name": "GI", "id": "GI"},
+                    ],
+                    data = [],  #this avoids displaying the nutrition_db on the screen
+                    row_deletable = True),
+
+            html.Div([
+                dcc.RadioItems(
+                    id='json-type',
+                    options=[{'label': 'Meals', 'value': 'meals'},
+                        {'label': 'Recipes', 'value': 'recipes'}
+                    ],
+                    value='meals', #default value
+                    labelStyle={'display': 'inline-block'}
                 )
             ])
-        ])
-        
+            ]   )
+    ])
+         
         return layout
+    
